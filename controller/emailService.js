2f1f5e6bfa35a481cfbe0ed59c0f3aa07aa813e3
maharsh patel <154405062+maharsh-1803@users.noreply.github.com>
2024-08-02 17:04:37 +0530

    last commit

--- controller/emailService.js
+++ controller/emailService.js
@@ -9,6 +9,8 @@ const transporter = nodemailer.createTransport({
     user: 'zoila.corkery@ethereal.email', // when you check you need to go to ethereal nodemail website and generate user for you at that time  and pass
     pass: '7UYUEzUgQTyeMx4V5' // when you check you need to go to ethereal nodemail website and generate pass for you at that time and pass
   }
+  // when you check you might get error  Error: Invalid login: 535 Authentication failed but this error is not in my code this error in ethereal , you have to generate new user and pass may be error will solve 
+  // you can get mail in ethereal mail box not in original mail id , we can you sendgrid for getting original mail 
 });
 
 const sendEmail = (to, subject, text) => {
